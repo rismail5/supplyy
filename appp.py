5667a51bcc370e89bc392d33042eb17475b23c63
rismail5 <rmohammedismail5@gmail.com>
2026-07-11 22:15:27 +0530

    Remove Streamlit app for delay prediction
    Remove the supply chain delay prediction dashboard code.

--- appp.py
+++ appp.py
@@ -1,13 +1,1 @@
-import streamlit as st
-import pandas as pd
-import joblib
 
-model = joblib.load('delay_model.pkl')
-st.title("Supply Chain Delay Prediction Dashboard")
-
-uploaded_file = st.file_uploader("Upload shipment data", type=["xlsx"])
-if uploaded_file:
-    data = pd.read_excel(uploaded_file)
-    data_encoded = pd.get_dummies(data)
-    prediction = model.predict(data_encoded)
-    st.write(prediction)
